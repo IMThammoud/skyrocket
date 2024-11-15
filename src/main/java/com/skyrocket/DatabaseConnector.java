d3d57649b1725b2bdf744b0456586a0aa3bb5ce3
hassan <trondl-github@proton.me>
2024-11-15 16:08:16 +0100

    made the DBconnector static

--- src/main/java/com/skyrocket/DatabaseConnector.java
+++ src/main/java/com/skyrocket/DatabaseConnector.java
@@ -11,27 +11,31 @@ import java.util.Properties;
 
 import static com.skyrocket.controller.PageController.LOG;
 
-public class DatabaseConnector {
-    private String url;
-    private String name;
-    private String pw;
-    static Properties props = new Properties();
-    public static Connection connection;
+public class DatabaseConnector{
+    static class DBConnector {
+        private String url;
+        private String name;
+        private String pw;
+        static Properties props = new Properties();
+        public static Connection connection;
 
-    public static void connect() {
-        try {
-            Path pathToEnvs = Paths.get("src/main/java/com/skyrocket/envs.txt");
-            InputStream file = Files.newInputStream(pathToEnvs);
-            props.load(file);
-        }catch (IOException e) {
-            System.out.println("Unable to read envs.txt file");
-        }
-        try {
-            connection = DriverManager.getConnection(props.get("DATABASE_URL") + ""+ props.get("DATABASE_NAME") +"?user="+ props.get("MARIADB_USERNAME") +"&password="+ props.get("MARIADB_PASSWORD"));
-            LOG.info("CONNECTION TO DATABASE SUCCESSFUL");
-        } catch (Exception e) {
-            LOG.info("DATABASE CONNECTION FAILED, REASON: "+ e.getMessage());
-        }
+        public static void connect() {
+            try {
+                Path pathToEnvs = Paths.get("src/main/java/com/skyrocket/envs.txt");
+                InputStream file = Files.newInputStream(pathToEnvs);
+                props.load(file);
+            }catch (IOException e) {
+                System.out.println("Unable to read envs.txt file");
+            }
+            try {
+                connection = DriverManager.getConnection(props.get("DATABASE_URL") + ""+ props.get("DATABASE_NAME") +"?user="+ props.get("MARIADB_USERNAME") +"&password="+ props.get("MARIADB_PASSWORD"));
+                LOG.info("CONNECTION TO DATABASE SUCCESSFUL");
+            } catch (Exception e) {
+                LOG.info("DATABASE CONNECTION FAILED, REASON: "+ e.getMessage());
+            }
 
+        }
     }
+
+
 }
