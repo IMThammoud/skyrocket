515b139bac9973db9314329453b8fca82ca46e25
hassan <trondl-github@proton.me>
2024-12-02 17:30:40 +0100

    added header-rows to the shelve table

--- src/main/resources/static/js/retrieveshelves.js
+++ src/main/resources/static/js/retrieveshelves.js
@@ -1,5 +1,6 @@
 // Don't forget to set the IPv4 of the server into the fetch request later
-
+// Dont forget to implement a cap of Shelves because there shouldt be 100 Shelves rendered
+// on one Page. Would be better to dynamically load them on button press "Next" or something
 async function retrieveShelves() {
     let request =   await fetch("http://localhost:8080/shelve/retrieve", {
                                 method : "POST",
@@ -8,11 +9,8 @@ async function retrieveShelves() {
 
     let response_array = JSON.parse(response);
 
-    document.getElementById("shelve-table").createTHead().innerText = "Regal"
-    document.getElementById("shelve-table").createTHead().innerText = "ID"
-    document.getElementById("shelve-table").createTHead().innerText = "Typ"
-    document.getElementById("shelve-table").createTHead().innerText = "Kategorie"
 
+    // Cycling through the retrieved shelves and building a table from it
     for (let i = 0; i < response_array.length ; i++) {
 
         let row = document.getElementById("shelve-table").insertRow(i);
@@ -29,5 +27,20 @@ async function retrieveShelves() {
         cell_category.innerText = response_array[i]["category"]
 
     }
+    // Adding header cells to the table
+    // Probably need to put this at the end otherwise the tableheaders are at the last row
+    let header_row = document.getElementById("shelve-table").insertRow(0)
+    let header_first =  header_row.insertCell(0)
+    let header_second = header_row.insertCell(1)
+    let header_third = header_row.insertCell(2)
+    let header_fourth = header_row.insertCell(3)
+
+    header_row.style.fontWeight = "bold"
+    header_first.innerText = "Regal"
+    header_second.innerText = "ID"
+    header_third.innerText = "Typ"
+    header_fourth.innerText = "Kategorie"
+
+
 
 }
