b107e2642200d236211665de7d092735754d485e
hassan <trondl-github@proton.me>
2024-12-15 01:16:44 +0100

    Added new article type "akku" and changed addArticleFunctions a bit

---
+++ src/main/java/com/skyrocket/model/articles/Akku.java
@@ -0,0 +1,21 @@
+package com.skyrocket.model.articles;
+
+import java.util.UUID;
+
+import com.skyrocket.Article;
+
+public class Akku extends Article {
+    
+    public Akku(UUID id,
+                    String name,
+                    int amount,
+                    String type,
+                    String description,
+                    double priceWhenBought,
+                    double sellingPrice,
+                    UUID shelveIdAsForeignKey){
+        super(id, name, amount, type, description, priceWhenBought, sellingPrice, shelveIdAsForeignKey);
+        
+
+    }
+}
--- src/main/resources/static/js/addArticleFunctions.js
+++ src/main/resources/static/js/addArticleFunctions.js
@@ -9,6 +9,7 @@ async function loadShelves(){
 
     for (const shelvesAsArrayKey in shelvesAsArray) {
         let option = document.createElement("option")
+        // Giving the option an ID so i can pass it to the server to get the right article-template back.
         option.value = shelvesAsArray[shelvesAsArrayKey]["id"]
         option.innerText = shelvesAsArray[shelvesAsArrayKey]["name"]
         document.getElementById("shelve").appendChild(option)
