6282fab68d96451df8c5edd46b6d2aea3d8a38b2
hassan <trondl-github@proton.me>
2024-12-16 19:22:48 +0100

    added nav bar to pages and added junit to project

--- src/test/java/com/skyrocket/SkyrocketApplicationTests.java
+++ src/test/java/com/skyrocket/SkyrocketApplicationTests.java
@@ -1,13 +1,23 @@
 package com.skyrocket;
 
+import com.skyrocket.controller.DynamicElementsController;
+import com.skyrocket.services.UserAccountQueries;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class SkyrocketApplicationTests {
 
+	@Mock
+	UserAccountQueries userAccountQueries;
+
+	@Mock
+	DynamicElementsController dynamicElementsController;
+
 	@Test
 	void contextLoads() {
 	}
-
 }
