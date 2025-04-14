d6afd9843e36e420cfa999a2e6488340f4d13c31
hassan <trondl-github@proton.me>
2025-04-14 20:02:55 +0200

    added check for shelve dashboard to display a text when no shelve is found and only cycle through shelve array if there are shelves

--- src/main/resources/static/js/showShelvesAsTable.js
+++ src/main/resources/static/js/showShelvesAsTable.js
@@ -8,66 +8,68 @@ extra_button.id = "extra_button";
 // extra_button.innerHTML = '<img style ="width: 30px" src="/icons/icons-skyrocket/eye-solid.svg" alt="listing-button">';
 extra_button.innerText = "View Articles";
 async function showShelvesAsTable() {
-    let request =   await fetch("http://localhost:8080/shelve/retrieve", {
-                                method : "POST",
+    let request = await fetch("http://localhost:8080/shelve/retrieve", {
+        method: "POST",
     })
-    let response =  await request.text();
+    let response = await request.text();
 
-    let response_array = JSON.parse(response);
-    console.log(response_array[0]["id"]);
+    let response_array = JSON.parse(response)
+    if (response_array.length > 0){
+        // Cycling through the retrieved shelves and building a table from it
+        for (let i = 0; i < response_array.length; i++) {
+            let shelve_id_of_current_iteration = response_array[i]["id"];
+            console.log(shelve_id_of_current_iteration);
+            let shelve_type_of_current_iteration = response_array[i]["type"];
 
-    // Cycling through the retrieved shelves and building a table from it
-    for (let i = 0; i < response_array.length ; i++) {
-        let shelve_id_of_current_iteration = response_array[i]["id"];
-        console.log(shelve_id_of_current_iteration);
-        let shelve_type_of_current_iteration = response_array[i]["type"];
+            let row = document.getElementById("shelve-table").insertRow(i);
 
-        let row = document.getElementById("shelve-table").insertRow(i);
+            let cell_name = row.insertCell(0)
+            let cell_type = row.insertCell(1)
+            let cell_category = row.insertCell(2)
+            let cell_articleCount = row.insertCell(3)
+            let cell_extra_button = row.insertCell(4)
 
-        let cell_name = row.insertCell(0)
-        let cell_type = row.insertCell(1)
-        let cell_category = row.insertCell(2)
-        let cell_articleCount = row.insertCell(3)
-        let cell_extra_button = row.insertCell(4)
+            cell_name.innerText = response_array[i]["name"]
+            cell_type.innerText = response_array[i]["type"]
+            cell_category.innerText = response_array[i]["category"]
 
-        cell_name.innerText = response_array[i]["name"]
-        cell_type.innerText = response_array[i]["type"]
-        cell_category.innerText = response_array[i]["category"]
+            // This endpoint expects exactly these field names: shelve_id, shelve_type
+            let askForArticleCount = await fetch("http://localhost:8080/shelve/articleCount", {
+                headers: {"content-type": "application/json"},
+                method: "POST",
+                body: JSON.stringify({
+                    shelve_id: shelve_id_of_current_iteration,
+                    shelve_type: shelve_type_of_current_iteration,
+                }),
+            })
+            let responseCount = await askForArticleCount.json()
 
-        // This endpoint expects exactly these field names: shelve_id, shelve_type
-        let askForArticleCount =   await fetch("http://localhost:8080/shelve/articleCount", {
-            headers : {"content-type": "application/json"},
-            method : "POST",
-            body : JSON.stringify({
-                shelve_id : shelve_id_of_current_iteration,
-                shelve_type: shelve_type_of_current_iteration,
-            }),
-        })
-        let responseCount = await askForArticleCount.json()
+            cell_articleCount.innerText = responseCount;
+            cell_extra_button.value = response_array[i]["id"];
+            let button_to_view_articles = document.createElement('button');
+            button_to_view_articles.innerText = "einsehen";
+            button_to_view_articles.addEventListener("click", async function () {
+                // Submit ShelveID through button and get Listing template for shelve.
+                // check if there are even Articles for that shelve
+                let data_articles = await listArticles(cell_extra_button)
+                if (data_articles.length == 0) {
+                    console.log("No articles found for this shelve")
+                    alert("Es sind keine Artikel in diesem Regal vorhanden.")
+                } else {
+                    console.log(data_articles)
+                    console.log("amount of keys (columns): ", Object.keys(data_articles[0]).length);
+                    replaceShelveDashboardWithArticleList(Object.keys(data_articles).length, data_articles);
+                }
 
-        cell_articleCount.innerText = responseCount;
-        cell_extra_button.value = response_array[i]["id"];
-        let button_to_view_articles = document.createElement('button');
-        button_to_view_articles.innerText = "einsehen";
-        button_to_view_articles.addEventListener("click", async function () {
-            // Submit ShelveID through button and get Listing template for shelve.
-            // check if there are even Articles for that shelve
-            let data_articles = await listArticles(cell_extra_button)
-            if (data_articles.length == 0){
-                console.log("No articles found for this shelve")
-                alert("Es sind keine Artikel in diesem Regal vorhanden.")
-            } else {
-                console.log(data_articles)
-                console.log("amount of keys (columns): ", Object.keys(data_articles[0]).length);
-                replaceShelveDashboardWithArticleList(Object.keys(data_articles).length, data_articles);
-            }
-
-        })
-        cell_extra_button.append(button_to_view_articles)
-
+            })
+            cell_extra_button.append(button_to_view_articles)
 
 
-
+        }
+} else {
+        let headlineIfNoShelvesAreAvailable = document.createElement("h6")
+        headlineIfNoShelvesAreAvailable.innerText = "Please create a shelve first. They will be listed here."
+        document.getElementById("shelve-table").appendChild(headlineIfNoShelvesAreAvailable)
     }
     // Adding header cells to the table
     // Probably need to put this at the end otherwise the tableheaders are at the last row
