6315c68e02630768687bacd82a5604d1c6776536
hassan <trondl-github@proton.me>
2025-04-30 00:58:02 +0200

    added articlebox add button to invoice dashboard and function that creates a new article box whenever the button is pressed.

---
+++ src/main/resources/static/js/invoice_dashboard_functions.js
@@ -0,0 +1,25 @@
+// used to increment IDs of the new article box that is added everytime user clicks the button..
+let counter = 1
+
+function add_article_box_and_increment_counter() {
+    // This element spawns into an Article Box in invoice_dashboard when a button is pressed to add another article
+    let article_box = document.createElement("div")
+    article_box.id = "article_box_" + counter
+    article_box.innerHTML =
+        "            <div>\n" +
+        "                <label for=\"Invoice_Id_"+ counter +"\">Invoice ID</label>\n" +
+        "                <input type=\"text\" id='Invoice_Id_"+ counter +"' placeholder=\"0001IFM\">\n" +
+        "            </div>\n" +
+        "\n" +
+        "            <div>\n" +
+        "                <label for=\"text_area_"+ counter +"\">Article / Service description</label>\n" +
+        "                <textarea name=\"\" id=\"text_area_"+ counter +"\" cols=\"30\" rows=\"10\" placeholder=\"1x Article with...\"></textarea>\n" +
+        "            </div>\n" +
+        "            <div>\n" +
+        "                <label for=\"price_"+ counter +"\">Price in € (tax included)</label>\n" +
+        "                <input type=\"number\" id=\"price_"+ counter +"\">\n" +
+        "            </div>\n"
+    document.getElementById("outer_div_4_to_add_articlebox_to").append(article_box)
+    console.log("Adding article box, and increasing counter..")
+    counter++
+}
