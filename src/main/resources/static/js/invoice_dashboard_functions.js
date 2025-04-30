2aa1e6e0b757464276ad13088af50e26edf09a24
hassan <trondl-github@proton.me>
2025-04-30 11:35:00 +0200

    Added alert box when article box was added through button click. added condititon check when adding article boxes (using counter) to prevent spamming boxes.

--- src/main/resources/static/js/invoice_dashboard_functions.js
+++ src/main/resources/static/js/invoice_dashboard_functions.js
@@ -4,23 +4,29 @@ let counter = 1
 
 function add_article_box_and_increment_counter() {
     // This element spawns into an Article Box in invoice_dashboard when a button is pressed to add another article
-    let article_box = document.createElement("div")
-    article_box.id = "article_box_" + counter
-    article_box.innerHTML =
-        "            <div>\n" +
-        "                <label for=\"Invoice_Id_"+ counter +"\">Invoice ID</label>\n" +
-        "                <input type=\"text\" id='Invoice_Id_"+ counter +"' placeholder=\"0001IFM\">\n" +
-        "            </div>\n" +
-        "\n" +
-        "            <div>\n" +
-        "                <label for=\"text_area_"+ counter +"\">Article / Service description</label>\n" +
-        "                <textarea name=\"\" id=\"text_area_"+ counter +"\" cols=\"30\" rows=\"10\" placeholder=\"1x Article with...\"></textarea>\n" +
-        "            </div>\n" +
-        "            <div>\n" +
-        "                <label for=\"price_"+ counter +"\">Price in € (tax included)</label>\n" +
-        "                <input type=\"number\" id=\"price_"+ counter +"\">\n" +
-        "            </div>\n"
-    document.getElementById("outer_div_4_to_add_articlebox_to").append(article_box)
-    console.log("added articlebox: " + article_box.id)
-    counter++
+    // only make button add article when counter is less than 6
+    if(counter < 6) {
+        let article_box = document.createElement("div")
+        article_box.id = "article_box_" + counter
+        article_box.innerHTML = "<br>" + "<br>" +
+            "            <div>\n" +
+            "                <label for=\"Invoice_Id_" + counter + "\">Invoice ID</label>\n" +
+            "                <input type=\"text\" id='Invoice_Id_" + counter + "' placeholder=\"0001IFM\">\n" +
+            "            </div>\n" +
+            "\n" +
+            "            <div>\n" +
+            "                <label for=\"text_area_" + counter + "\">Article / Service description</label>\n" +
+            "                <textarea name=\"\" id=\"text_area_" + counter + "\" cols=\"30\" rows=\"10\" placeholder=\"1x Article with...\"></textarea>\n" +
+            "            </div>\n" +
+            "            <div>\n" +
+            "                <label for=\"price_" + counter + "\">Price in € (tax included)</label>\n" +
+            "                <input type=\"number\" id=\"price_" + counter + "\">\n" +
+            "            </div>\n"
+        document.getElementById("outer_div_4_to_add_articlebox_to").append(article_box)
+        console.log("added articlebox: " + article_box.id)
+        counter++
+        alert("Added article box.")
+    } else {
+        alert("Already added 5 Article Boxes.")
+    }
 }
